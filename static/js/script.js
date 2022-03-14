ac4c8f6ca95c574b4185bb58bad322d44d1763a2
Christopher De Vries <devries@idolstarastronomer.com>
2022-03-14 09:30:55 -0400

    Get rid of second password box and enlarge submit button

--- static/js/script.js
+++ static/js/script.js
@@ -5,29 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function submitPassword() {
   var pw1 = document.getElementById("password1").value;
-  var pw2 = document.getElementById("password2").value;
 
-  if (pw1 !== pw2) {
-    document.getElementById("response").innerHTML = "Passwords do not match";
-  } else {
-    var url = new URL("https://purgatory-j6z4gxi7tq-uc.a.run.app");
-    var params = {"password": pw1};
-    url.search = new URLSearchParams(params).toString();
+  var url = new URL("https://purgatory-j6z4gxi7tq-uc.a.run.app");
+  var params = {"password": pw1};
+  url.search = new URLSearchParams(params).toString();
 
-    fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        document.getElementById("response").innerHTML = data.message;
-      })
-      .catch(error => {
-        console.log(error);
-        document.getElementById("response").innerHTML = "Something went wrong";
-      })
-      .finally(() => {
-        document.getElementById("password1").value = "";
-        document.getElementById("password2").value = "";
-      });
-  }
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      document.getElementById("response").innerHTML = data.message;
+    })
+    .catch(error => {
+      console.log(error);
+      document.getElementById("response").innerHTML = "Something went wrong";
+    })
+    .finally(() => {
+      document.getElementById("password1").value = "";
+    });
 }
